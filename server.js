b6e86b7c7687444bb43ea385ed51d9a34efe3bec
alejandromg <vamg008@gmail.com>
2012-07-13 00:26:27 -0600

    [fix] Try this?

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ var less = require('less');
 var lessMiddleware = require('less-middleware');
 var server = express.createServer();
 var nowjs = require("now");
- scripts = [
+var everyone = '';
+var scripts = [
    'javascripts/jquery.js',
    'javascripts/easel.js',
    'javascripts/script.js'
@@ -193,150 +194,152 @@ app.get('/logout', function(req, res){
 
 //server.listen(3000,"laruche.local");
 everyauth.helpExpress(app);
-app.listen(process.env['app_port'] || 3000);
+app.listen(process.env['app_port'] || 3000, function (){
+  //Init Nowjs
+  everyone = nowjs.initialize(app); //Main 
+  var post_everyone = nowjs.getGroup('everyone');
+  var group = nowjs.getGroup('everyone');
 
-//Init Nowjs
-var everyone = nowjs.initialize(app); //Main 
-var post_everyone = nowjs.getGroup('everyone');
-var group = nowjs.getGroup('everyone');
-
-//Handling connect and disconnect
-everyone.on('leave', function() { 
-    message=' is disconnected from chat';
-    console.log('Disconnected from server'); 
-    --counter;
-    link=this.now.name;
-    if(this.now.network=='facebook')
-    link='<a target="_blank" href="http://www.facebook.com/'+this.now.username+'">'+this.now.name+'</a>';
-    else
-    if(this.now.network=='twitter')
-    link='<a target="_blank" href="http://www.twitter.com/'+this.now.username+'">'+this.now.name+'</a>';
-    else
-    if(this.now.network=='google+')
-    link='<a target="_blank" href="https://plus.google.com/'+this.now.username+'">'+this.now.name+'</a>';
-    everyone.now.receiveMessage(link, message,7); 
-   console.log('this.user.clientId %s',this.user.clientId);
-   if(this.user.clientId){
-   //everyone.removeUser(this.user.clientId);
-    users[this.now.name] = null;
-  }
-   everyone.count(function (ct) {
-	    everyone.now.countUsers(ct);
-   });
-   console.log('this.now  = %j',this.now)
-   delete this.now;
-   console.log('Hiring disconnect');
-}); 
-everyone.on('connect', function() { 
-  console.log('Hiring connect');
-  this.now.receivePrevMessage(lastmsgs.toString());   
-  trie.addWord(rootVert, this.now.name);
-  if(this.user.clientId){
-    everyone.addUser(this.user.clientId);
-    users[this.now.name] = this.user.clientId;
-    if(cusers[this.user.clientId]==null)
-      message=' is connected to chat via '+this.now.network;
-    cusers[this.user.clientId] = this.now.name;
-  }
-    console.log('connected to server'); 
-    ++counter;
-    everyone.count(function (ct) {
-	    everyone.now.countUsers(ct);
-	});
-    //Date.now() = mill sec since 1970
-    link=this.now.name;
-    if(this.now.network=='facebook')
-    link='<a target="_blank" href="http://www.facebook.com/'+this.now.username+'">'+this.now.name+'</a>';
-    else
-    if(this.now.network=='twitter')
-    link='<a target="_blank" href="http://www.twitter.com/'+this.now.username+'">'+this.now.name+'</a>';
-    else
-    if(this.now.network=='google+')
-    link='<a target="_blank" href="https://plus.google.com/'+this.now.username+'">'+this.now.name+'</a>';
-    everyone.now.receiveMessage(link, message,7);
-  var usernames=[];
-  everyone.getUsers(function (users) { 
-	for (var i = 0; i < users.length; i++)
-	      usernames.push("<br>"+cusers[users[i]]+"");
-  });
-  everyone.now.receiveUserList(usernames.toString());   
-}); 
-//Nowjs
-group.now.distributeMessage = function(message){
-  console.log('Group:'+message);
-};
-//Depreciated
-everyone.now.startChat = function(message){
-  console.log("Hiring StartChat");
-  this.now.receivePrevMessage(lastmsgs.toString());   
-  trie.addWord(rootVert, this.now.name);
-  if(this.user.clientId){
-    console.log(this.user.clientId);
-    everyone.addUser(this.user.clientId);
-    users[this.now.name] = this.user.clientId;
-  }
-};
-
-everyone.logOut= function() {
-     console.log('Hiring logout function');
-   delete this.now;
-};
-
-everyone.setUser= function(user) {
-  this.now.name=user.name;
-  this.now.username=user.username;
-  this.now.network=user.network;
-};
-everyone.getUsers(function (users) {
-  for (var i = 0; i < users.length; i++) 
-   console.log(users[i]);
-});
-getUserNames = function (cusers) {
-var usernames=[];
-for (var i = 0; i < cusers.length; i++){
-  nowjs.hasClient(cusers[i], function (bool) { 
-    if (bool) { 
-      usernames.push(cusers[i]);
-    } 
-  });
-console.log(cusers.toString());
-}
-};
-//get and post message
-everyone.now.distributeMessage = function(to_whom,message){//,prevmessages){
-  console.log('benim id: %s adim %s',this.user.clientId,this.now.name);
-  from_user = cusers[this.user.clientId];
-  nowjs.getClient(this.user.clientId, function(data){
-            console.log('getClient %j',this.now.name);
-  }); 
-  if(to_whom){
-    if(users[to_whom]){
-    console.log('to whom:'+to_whom+' '+users[to_whom]);
-    nowjs.getClient(users[to_whom], function(data){
-	    console.log('callback on to whom');
-	    this.now.receiveMessage(from_user, message,-1); //private
-	    }); 
-    this.now.receiveMessage('Me -> '+to_whom, message,0); //me
+  //Handling connect and disconnect
+  everyone.on('leave', function() { 
+      message=' is disconnected from chat';
+      console.log('Disconnected from server'); 
+      --counter;
+      link=this.now.name;
+      if(this.now.network=='facebook')
+      link='<a target="_blank" href="http://www.facebook.com/'+this.now.username+'">'+this.now.name+'</a>';
+      else
+      if(this.now.network=='twitter')
+      link='<a target="_blank" href="http://www.twitter.com/'+this.now.username+'">'+this.now.name+'</a>';
+      else
+      if(this.now.network=='google+')
+      link='<a target="_blank" href="https://plus.google.com/'+this.now.username+'">'+this.now.name+'</a>';
+      everyone.now.receiveMessage(link, message,7); 
+     console.log('this.user.clientId %s',this.user.clientId);
+     if(this.user.clientId){
+     //everyone.removeUser(this.user.clientId);
+      users[this.now.name] = null;
     }
-    else 
-    this.now.receiveMessage('There is no user with name ', to_whom,7); //custom
+     everyone.count(function (ct) {
+        everyone.now.countUsers(ct);
+     });
+     console.log('this.now  = %j',this.now)
+     delete this.now;
+     console.log('Hiring disconnect');
+  }); 
+  everyone.on('connect', function() { 
+    console.log('Hiring connect');
+    this.now.receivePrevMessage(lastmsgs.toString());   
+    trie.addWord(rootVert, this.now.name);
+    if(this.user.clientId){
+      everyone.addUser(this.user.clientId);
+      users[this.now.name] = this.user.clientId;
+      if(cusers[this.user.clientId]==null)
+        message=' is connected to chat via '+this.now.network;
+      cusers[this.user.clientId] = this.now.name;
+    }
+      console.log('connected to server'); 
+      ++counter;
+      everyone.count(function (ct) {
+        everyone.now.countUsers(ct);
+    });
+      //Date.now() = mill sec since 1970
+      link=this.now.name;
+      if(this.now.network=='facebook')
+      link='<a target="_blank" href="http://www.facebook.com/'+this.now.username+'">'+this.now.name+'</a>';
+      else
+      if(this.now.network=='twitter')
+      link='<a target="_blank" href="http://www.twitter.com/'+this.now.username+'">'+this.now.name+'</a>';
+      else
+      if(this.now.network=='google+')
+      link='<a target="_blank" href="https://plus.google.com/'+this.now.username+'">'+this.now.name+'</a>';
+      everyone.now.receiveMessage(link, message,7);
+    var usernames=[];
+    everyone.getUsers(function (users) { 
+    for (var i = 0; i < users.length; i++)
+          usernames.push("<br>"+cusers[users[i]]+"");
+    });
+    everyone.now.receiveUserList(usernames.toString());   
+  }); 
+  //Nowjs
+  group.now.distributeMessage = function(message){
+    console.log('Group:'+message);
+  };
+  //Depreciated
+  everyone.now.startChat = function(message){
+    console.log("Hiring StartChat");
+    this.now.receivePrevMessage(lastmsgs.toString());   
+    trie.addWord(rootVert, this.now.name);
+    if(this.user.clientId){
+      console.log(this.user.clientId);
+      everyone.addUser(this.user.clientId);
+      users[this.now.name] = this.user.clientId;
+    }
+  };
+
+  everyone.logOut= function() {
+       console.log('Hiring logout function');
+     delete this.now;
+  };
+
+  everyone.setUser= function(user) {
+    this.now.name=user.name;
+    this.now.username=user.username;
+    this.now.network=user.network;
+  };
+  everyone.getUsers(function (users) {
+    for (var i = 0; i < users.length; i++) 
+     console.log(users[i]);
+  });
+  getUserNames = function (cusers) {
+  var usernames=[];
+  for (var i = 0; i < cusers.length; i++){
+    nowjs.hasClient(cusers[i], function (bool) { 
+      if (bool) { 
+        usernames.push(cusers[i]);
+      } 
+    });
+  console.log(cusers.toString());
   }
-  else{
-  everyone.now.receiveMessage(from_user, message,1); //public
-  }
-};
-everyone.now.updatePrevMessages = function(message){
-lastmsgs.push(message);
-while(lastmsgs.length>10)
-lastmsgs.shift();
-};
-//Autocomplete
-everyone.now.getGuess = function(val) {
-  //val = val.toLowerCase();
-  var guesses = trie.retrieve(val);
-  console.log('%s getGuess -> receiveGuess %j',val,guesses[0]);
-  this.now.receiveGuess(guesses[0]);
-};
+  };
+  //get and post message
+  everyone.now.distributeMessage = function(to_whom,message){//,prevmessages){
+    console.log('benim id: %s adim %s',this.user.clientId,this.now.name);
+    from_user = cusers[this.user.clientId];
+    nowjs.getClient(this.user.clientId, function(data){
+              console.log('getClient %j',this.now.name);
+    }); 
+    if(to_whom){
+      if(users[to_whom]){
+      console.log('to whom:'+to_whom+' '+users[to_whom]);
+      nowjs.getClient(users[to_whom], function(data){
+        console.log('callback on to whom');
+        this.now.receiveMessage(from_user, message,-1); //private
+        }); 
+      this.now.receiveMessage('Me -> '+to_whom, message,0); //me
+      }
+      else 
+      this.now.receiveMessage('There is no user with name ', to_whom,7); //custom
+    }
+    else{
+    everyone.now.receiveMessage(from_user, message,1); //public
+    }
+  };
+  everyone.now.updatePrevMessages = function(message){
+  lastmsgs.push(message);
+  while(lastmsgs.length>10)
+  lastmsgs.shift();
+  };
+  //Autocomplete
+  everyone.now.getGuess = function(val) {
+    //val = val.toLowerCase();
+    var guesses = trie.retrieve(val);
+    console.log('%s getGuess -> receiveGuess %j',val,guesses[0]);
+    this.now.receiveGuess(guesses[0]);
+  };
+});
+
+
 
  function Trie(vertex) {
   this.root = vertex;
